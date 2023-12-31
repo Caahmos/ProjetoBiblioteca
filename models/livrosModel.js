4429bcef72241f08c18e1ec0e6800fac51251bcf
Alex <dev.alexmorales@gmail.com>
2023-12-31 14:51:26 -0300

    Tratando Possíveis Injeções de SQL

--- models/livrosModel.js
+++ models/livrosModel.js
@@ -2,52 +2,62 @@ const pool = require('../db/conne');
 
 class Livros {
     constructor(body) {
-            this.body = body,
+        this.body = body,
             this.errors = [],
             this.user = null
     }
 
     cadastrar() {
-        const sqlQuery = `INSERT INTO books (title, pages) VALUES ('${this.body.title}', '${this.body.pages}')`;
-        pool.query(sqlQuery, (err) => {
+        const sqlQuery = `INSERT INTO books (??, ??) VALUES (?, ?)`;
+
+        const data = ['title', 'pages', this.body.title, this.body.pages];
+
+        pool.query(sqlQuery, data, (err) => {
             console.log(err);
         })
     }
 
-    atualizar(id){
-        const sqlQuery = `UPDATE books SET title = '${this.body.title}', pages = '${this.body.pages}' WHERE id = ${id}`;
-        pool.query(sqlQuery, (err) => {
+    atualizar(id) {
+        const sqlQuery = `UPDATE books SET ?? = ?, ?? = ? WHERE ?? = ?`;
+
+        const data = ['title', this.body.title, 'pages', this.body.pages, 'id', id];
+
+        pool.query(sqlQuery, data, (err) => {
             console.log(err);
         })
     }
 
-    static exibirLivros(callback){
+    static exibirLivros(callback) {
         const sqlQuery = 'SELECT * FROM books';
-        
+
         pool.query(sqlQuery, (err, data) => {
-            if(err) console.log(err);
+            if (err) console.log(err);
 
             const livros = data;
             callback(null, livros);
         })
     }
 
-    static exibirLivrosporId(id, callback){
-        const sqlQuery = `SELECT * FROM books WHERE id=${id}`;
-        
-        pool.query(sqlQuery, (err, data) => {
-            if(err) console.log(err);
+    static exibirLivrosporId(id, callback) {
+        const sqlQuery = `SELECT * FROM books WHERE ?? = ?`;
+
+        const data = ['id', id];
+
+        pool.query(sqlQuery, data, (err, data) => {
+            if (err) console.log(err);
 
             const livros = data;
             callback(null, livros);
         })
     }
 
-    static removerLivro(id){
-        const sqlQuery = `DELETE FROM books WHERE id = ${id}`;
+    static removerLivro(id) {
+        const sqlQuery = `DELETE FROM books WHERE ?? = ?`;
 
-        pool.query(sqlQuery, (err) => {
-            if(err) console.log(err);
+        const data = ['id', id];
+
+        pool.query(sqlQuery, data, (err) => {
+            if (err) console.log(err);
         })
     }
 }
